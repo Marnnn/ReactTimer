39b6bc1be860ebe1bb0591c664659d3049af8621
Graham Test Fletcher <GrFletcher@draperanddash.local>
2016-06-06 21:38:20 +0100

    Add style to clock

--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -4,12 +4,22 @@ var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
-var Clock =require('Clock');
+var Clock = require('Clock');
 
 describe('Clock', () => {
   it('should exist', () => {
     expect(Clock).toExist();
   });
+
+  describe('render', () => {
+    it('should render clock to output', () => {
+      var clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62}/>);
+      var $el = $(ReactDOM.findDOMNode(clock));
+      var actualText = $el.find('.clock-text').text();
+
+      expect(actualText).toBe('01:02');
+    });
+  )};
 
   describe('formatSeconds', () => {
     it('should format seconds', () => {
